a630e683fc3bdc26564ea0cd1da84aebdd189f24
Sean Grindal <sean37gl@gmail.com>
2022-09-12 14:55:36 -0400

    adding test case

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -25,9 +25,9 @@ public class IntroLabTest {
         assertEquals(expected, actual);
     }
 
-    /* TODO: Write a test case for the method you wrote in IntroLab.
-     *       If done properly, you should be able to run IntroLabTest and see
-     *       the test results.
-     * As a reference, we've included testExampleStudent above.
-     */
+    public void testSean() {
+        String expected = "Hi!! My name is Sean Grindal! I like dogs more than cats! Welcome.";
+        String actual = IntroLab.seanIntro();
+        assertEquals(expected, actual);
+    }
 }
